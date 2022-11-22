e30ad121fe7992990cd8b5966dd3a2d94d35b430
IamSerdar <programmer.asa@gmail.com>
2022-11-22 17:08:58 +0500

    add author api

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthorRequest;
+use App\Http\Resources\AuthorResource;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $authors = Author::all();
+        return $this->respondOK(
+            AuthorResource::collection($authors)
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AuthorRequest $request)
+    {
+        $author = Author::create($request->all());
+        return $this->respondCreated(
+            AuthorResource::make($author)
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Author $author)
+    {
+        $author->update($request->all());
+
+        return $this->respondUpdated(new AuthorResource($author));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        $author->delete();
+
+        return $this->respondDeleted();
+    }
+}
